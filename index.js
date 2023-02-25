463971fe125e086547c5a771d534bad61fd776e2
Douglas Filho <dodo242009@gmail.com>
2023-02-25 16:50:18 -0300

    criacao da estrutura base do codigo

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
 
-//Middleware para leitura
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3000;
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
 app.use(
   express.urlencoded({
     extended: true,
   })
-)
+);
 
-//Endpoints
 app.get('/', (req, res) => {
   res.json({
     message:'Ola, bem vindo!'
   })
-})
+});
 
-//Porta disponibilizada para chamada da api
-app.listen(3000)
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@mtg-analytics.uoglgts.mongodb.net/mtg_cards?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.info(`Aplicação rodando em http://localhost/${port}`)
+    })
+  })
+  .catch(() => {
+    console.info('Erro ao conectar ao banco!')
+  });
